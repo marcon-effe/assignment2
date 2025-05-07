0192192199c2cb261c3e7f0807468bb7cdf8ce85
AlbeUni <alberto.reginato.1@studenti.unipd.it>
2025-05-07 10:43:18 +0200

    Implementazione della funzione IntegerToRoman, close #3

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,7 +7,21 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        //TODO
-        return null;
+        if (number <= 0 || number > 1000) {
+            throw new IllegalArgumentException(
+                "Numero inserito esterno al range [1-1000]");
+        }
+
+        int[]    values  = {1000,  900, 500, 400, 100,  90,  50,  40,  10,   9,   5,   4,   1};
+        String[] symbols = {"M",  "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                roman.append(symbols[i]);
+                number -= values[i];
+            }
+        }
+        return roman.toString();
     }
 }
