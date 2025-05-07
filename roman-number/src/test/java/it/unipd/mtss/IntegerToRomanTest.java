763d2e23d0dfc04d3891242e188d57a1226b4f1e
AlbeUni <alberto.reginato.1@studenti.unipd.it>
2025-05-07 14:26:37 +0200

    Aggiunti i test per i numeri fino al 6, close #8

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -10,10 +10,16 @@ import org.junit.jupiter.api.Test;
 
 public class IntegerToRomanTest {
     @Test
-    public void testConvert() {
+    public void test1_3() {
         assertEquals("I", IntegerToRoman.convert(1));
         assertEquals("II", IntegerToRoman.convert(2));
         assertEquals("III", IntegerToRoman.convert(3));
+    }
 
+    @Test
+    public void test4_6() {
+        assertEquals("IV", IntegerToRoman.convert(4));
+        assertEquals("V", IntegerToRoman.convert(5));
+        assertEquals("VI", IntegerToRoman.convert(6));
     }
 }
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -10,9 +10,17 @@ import org.junit.jupiter.api.Test;
 
 public class RomanPrinterTest {
     @Test
-    public void testPrint() {
+    public void testPrint1_3() {
         assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
         assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
         assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
     }
+
+    @Test
+    public void testPrint4_6() {
+        assertEquals("  _____  __      __\n |_   _| \\ \\    / /\n   | |    \\ \\  / / \n   | |     \\ \\/ /  \n  _| |_     \\  /   \n |_____|     \\/    \n", RomanPrinter.print(4));
+        assertEquals(" __      __\n \\ \\    / /\n  \\ \\  / / \n   \\ \\/ /  \n    \\  /   \n     \\/    \n", RomanPrinter.print(5));
+        assertEquals(" __      __  _____ \n \\ \\    / / |_   _|\n  \\ \\  / /    | |  \n   \\ \\/ /     | |  \n    \\  /     _| |_ \n     \\/     |_____|\n", RomanPrinter.print(6)); 
+    }
+
 }
